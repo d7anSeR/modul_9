a55a61ffa16aa8d98e057ae893270ae2de60bcff
d7anSeR <kyspanova@icloud.com>
2025-10-04 14:26:51 +0400

    add modul9

---
+++ src/middle/third/Parking.java
@@ -0,0 +1,41 @@
+package middle.third;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+class Parking {
+    private int capacity;
+    private List<Car> cars;
+
+    Parking(int capacity) {
+        this.capacity = capacity;
+        this.cars = new ArrayList<>();
+    }
+
+    boolean push(Car car) {
+        if (cars.size() < capacity) {
+            cars.add(car);
+            System.out.println(car + " заехала на парковку. Занято мест: " + cars.size());
+            return true;
+        } else {
+            System.out.println("Нет мест! Машина " + car + " не может заехать.");
+            return false;
+        }
+    }
+
+    Car pop() {
+        if (!cars.isEmpty()) {
+            Car car = cars.remove(cars.size() - 1);
+            System.out.println(car + " уехала с парковки. Занято мест: " + cars.size());
+            return car;
+        } else {
+            System.out.println("Парковка пустая!");
+            return null;
+        }
+    }
+
+    void showParking() {
+        System.out.println("Сейчас на парковке: " + cars);
+    }
+}
